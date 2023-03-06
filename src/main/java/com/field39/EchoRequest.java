522704cbbd4cde8fb1dcc85eaa145e39a2e54c6a
yasiryounus <yasir.younus@gmail.com>
2023-03-06 20:00:18 +0300

    creating mocks for ascii, xml and ebcdic channels

--- src/main/java/com/field39/EchoRequest.java
+++ src/main/java/com/field39/EchoRequest.java
@@ -4,32 +4,80 @@ import java.io.IOException;
 
 import org.jpos.iso.ISOException;
 import org.jpos.iso.ISOMsg;
+import org.jpos.iso.ISOPackager;
 import org.jpos.iso.ISOUtil;
 import org.jpos.iso.channel.ASCIIChannel;
+import org.jpos.iso.channel.XMLChannel;
 import org.jpos.iso.packager.GenericPackager;
+import org.jpos.iso.packager.ISO87BPackager;
+import org.jpos.iso.packager.XMLPackager;
 
 public class EchoRequest {
 
 	public static void main(String[] args) throws IOException, ISOException {
 		
-		GenericPackager packager = new GenericPackager("src/dist/cfg/packager/isoebcdic-field39.xml");
+		GenericPackager packagerEBCDIC = new GenericPackager("src/dist/cfg/packager/isoebcdic-field39.xml");
+		ISO87BPackager packagerISO87 = new ISO87BPackager();
+		XMLPackager xmlPackager = new XMLPackager();
+	
 		
-		ISOMsg message = new ISOMsg();
+		ISOMsg ISOMsgEBCDIC = new ISOMsg();
+		ISOMsgEBCDIC = prepareISOMsg(packagerEBCDIC, ISOMsgEBCDIC);
 		
-		message.setPackager(packager);
-		message.setMTI("0800");
-		message.set(11, "123456");
-		message.set(41, "00000001");
-		message.set(70, "301");
 		
-		ASCIIChannel channel = new ASCIIChannel("localhost", 7000, packager);
+		ISOMsg ISOMsgB = new ISOMsg();
+		ISOMsgB = prepareISOMsg(packagerISO87, ISOMsgB);
 		
-		System.out.println(ISOUtil.hexdump(message.getBytes()));
 		
-		channel.connect();
-		channel.send(message);
+		ISOMsg ISOMsgXML = new ISOMsg();
+		ISOMsgXML = prepareISOMsg(xmlPackager, ISOMsgXML);
+		
+		
+		//System.out.println(ISOUtil.hexdump(ISOMsgEBCDIC.getBytes()));
+		print("EBCDIC presentation", ISOMsgEBCDIC);
+        
+//        System.out.println("Sending ebcdic message to localhost:9000\n\n");
+//		ASCIIChannel channelEBCDIC = new ASCIIChannel("localhost", 9000, packagerEBCDIC);
+//        channelEBCDIC.connect();
+//		channelEBCDIC.send(ISOMsgEBCDIC);
+        
+		print("ASCII presentation", ISOMsgB);
+		
+//        System.out.println("Sending ascii message to localhost:8000");
+//        ASCIIChannel channelASCII = new ASCIIChannel("localhost", 8000, packagerISO87);
+//        channelASCII.connect();
+//		channelASCII.send(ISOMsgXML);
+		
+		print("XML based packager", ISOMsgXML);
+		System.out.println("Sending XML based message to localhost:8000");
+//        XMLChannel channelASCII = new XMLChannel("localhost", 8000, xmlPackager);
+//        channelASCII.connect();
+//		channelASCII.send(ISOMsgXML);
+		
+		
+		
 		
 
+	}
+	
+	private static void print(String message, ISOMsg m) throws ISOException {
+		byte[] data = m.pack();
+		short messageLength = (short)data.length;
+		System.out.println("MLI" + messageLength);
+		System.out.println(message);
+        System.out.println(ISOUtil.hexdump(data));
+	}
+	
+	private static ISOMsg prepareISOMsg(ISOPackager p, ISOMsg m) throws ISOException {
+		
+		m.setPackager(p);
+		m.setMTI("0800");
+		m.set(11, "123456");
+		m.set(41, "00000001");
+		m.set(70, "301");
+		
+		return m;
+		
 	}
 
 }
